aac61d84a3df9444425963de05de1761c94a45ce
rasbin <rasbin.thapa@learnsic.com>
2025-02-13 16:59:08 +0545

    custom user added

--- core/settings.py
+++ core/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
     # apps
     'course',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -145,5 +146,7 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_LIFETIME_LATE_USER': timedelta(days=1),
     'SLIDING_TOKEN_LIFETIME_LATE_USER': timedelta(days=30),
 }
+
+AUTH_USER_MODEL = 'accounts.UserData'
 
 
